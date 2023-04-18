7a4b9141aecb3206bf6e8383ab00a534ffe45708
baleegh <ba1283@nyu.edu>
2023-04-18 06:00:00 +0000

    adding outputs

--- m2s-bench-parsec-3.0/runs.sh
+++ m2s-bench-parsec-3.0/runs.sh
@@ -3,65 +3,65 @@ echo "$NTHREADS"
 
 cd blackscholes
 pwd 
-m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output.txt
+m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output-FLRU.txt
 cd ..
 
 cd bodytrack
 pwd 
-m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output.txt
+m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output-FLRU.txt
 cd ..
 
 cd canneal
 pwd 
-m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output.txt
+m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output-FLRU.txt
 cd ..
 
 cd dedup
 pwd 
-m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output.txt
+m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output-FLRU.txt
 cd ..
 
 cd facesim
 pwd 
-m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output.txt
+m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output-FLRU.txt
 cd ..
 
 cd ferret
 pwd 
-m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output.txt
+m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output-FLRU.txt
 cd ..
 
 cd fluidanimate
 pwd 
-m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output.txt
+m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output-FLRU.txt
 cd ..
 
 cd freqmine
 pwd 
-m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output.txt
+m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output-FLRU.txt
 cd ..
 
 cd raytrace
 pwd 
-m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output.txt
+m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output-FLRU.txt
 cd ..
 
 cd streamcluster
 pwd 
-m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output.txt
+m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output-FLRU.txt
 cd ..
 
 cd swaptions
 pwd 
-m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output.txt
+m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output-FLRU.txt
 cd ..
 
 cd vips
 pwd 
-m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output.txt
+m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output-FLRU.txt
 cd ..
 
 cd x264
 pwd 
-m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output.txt
+m2s --mem-config mem-config --x86-config x86-config --max-time 1800 --ctx-config benchmark.ini &>output-FLRU.txt
 cd ..
